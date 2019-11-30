865b3509d7b7fcfecc293560375bfff1593cd917
pham-huyson87 <pham.huyson87@hotmail.com>
2019-11-30 18:26:23 -0500

    - Arrow functions

---
+++ arrow-functions.ts
@@ -0,0 +1,15 @@
+function getValue() {
+    return 10;
+}
+
+const getValueArrow = () => {
+    return 10;
+}
+
+const getValueArrowShorter = () => 10;
+
+
+
+console.log(getValue());
+console.log(getValueArrow());
+console.log(getValueArrowShorter());
---
+++ scripts/arrow-functions.js
@@ -0,0 +1,10 @@
+function getValue() {
+    return 10;
+}
+var getValueArrow = function () {
+    return 10;
+};
+var getValueArrowShorter = function () { return 10; };
+console.log(getValue());
+console.log(getValueArrow());
+console.log(getValueArrowShorter());
