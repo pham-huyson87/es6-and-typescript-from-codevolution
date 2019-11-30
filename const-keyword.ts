88e36fee6671af59dd7eafaf843fd236807d48bf
pham-huyson87 <pham.huyson87@hotmail.com>
2019-11-30 18:05:29 -0500

    - const subtilities

---
+++ const-keyword.ts
@@ -0,0 +1,14 @@
+let num1;
+
+//const num2;               // Impossible without initialize
+const num2 = 10;
+
+const person = {
+    name: "Huy"
+};
+
+//person = {};              // Impossible to modify the variable
+person.name = "Huy Son"     // Possible to modify the object properties
+
+
+console.log(person.name);
---
+++ scripts/const-keyword.js
@@ -0,0 +1,10 @@
+var num1;
+//const num2;               // Impossible without initialize
+var num2 = 10;
+var person = {
+    name: "Huy"
+};
+//person = {};              // Impossible to modify the variable
+person.name = "Huy Son"; // Possible to modify the object properties
+console.log(person.name);
+//# sourceMappingURL=const-keyword.js.map
